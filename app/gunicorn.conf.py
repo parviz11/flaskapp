75e5cac0d7e17ae13554b67257f49dc5053519f2
Pärviz <parviz.alizade@gmail.com>
2023-12-15 14:49:06 +0100

    changed port

--- app/gunicorn.conf.py
+++ app/gunicorn.conf.py
@@ -1,5 +1,5 @@
 import multiprocessing
 
-bind = "0.0.0.0:8000" # the network interface and port on which Gunicorn should bind and listen for incoming connections
+bind = "0.0.0.0:8080" # the network interface and port on which Gunicorn should bind and listen for incoming connections
 workers = (multiprocessing.cpu_count() * 2) + 1 # the number of worker processes that Gunicorn should spawn to handle incoming requests concurrently
 threads = 2 # the number of threads per worker process
